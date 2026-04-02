f0b85336ee299603086cfd3d4828e27bf3189f93
kail15 <mpresto14@gmail.com>
2026-04-02 13:34:27 -0300

    Pseudocodigo 1 y 2
    Se agrega pseudocodigo ejs 1 y 2

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -7,6 +7,19 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    /*
+    Definir variable ganador
+    Definir variable puntajeMaximo
+    Mientras hayan rondas O(N)
+        Buscar la posicion del participante O(1)
+        Busco el participante de la ronda O(1)
+        Si lo encuentro -> Le sumo el puntaje O(1)
+        Si no -> Lo agrego O(1)
+        
+        Busco el participante de la ronda O(1)
+        Comparo si es mayor al puntajeMaximo
+        Si es mayor -> Actualizo puntajeMaximo y ganador
+    Devuelvo ganador
+    */
     return 0;
 }
