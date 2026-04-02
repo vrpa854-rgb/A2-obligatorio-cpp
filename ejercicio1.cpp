75b92c77beeb908bb73e926a148acef67b09f388
Venancio <v_r_p_a@hotmail.com>
2026-04-02 13:19:46 -0300

    Pseudocodigo de Ej 1 y 2

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -7,5 +7,18 @@ using namespace std;
 
 int main()
 {
+    /*
+    Recorrer el string
+    Separar en array
+    Definimos un contador para solicitar llave
+    Detectar llave y puerta
+    Mientras existan puertas
+        Si llave en mayuscula != puerta
+            Busco en llavero la llave
+            Si no lo encuentro
+                Solicito llave -> sumo 1 al contador
+                Guardo la llave en el llavero
+    Devuelvo contador
+    */
     return 0;
 }
